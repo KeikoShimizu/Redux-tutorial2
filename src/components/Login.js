48847f362ed5c91a895d8c7ac5c3ef6e83d361ad
KEIKO SHIMIZU <106935804+KeikoShimizu@users.noreply.github.com>
2023-04-27 23:12:45 -0700

    new code

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { login, logout } from '../features/user'
+
 
 const Login = () => {
+    const dispatch = useDispatch();
   return (
     <div>
-      <button>Login</button>
+        <button onClick={() => {
+            dispatch(login({name: "Jake", age: 20, email: "jakie@gmail.com"}))
+            }}>Login
+        </button>
+        <button onClick={() => {
+            dispatch(logout())
+            }}>Logout
+        </button>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -5,13 +5,17 @@ import reportWebVitals from './reportWebVitals';
 import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux';
 
+import userReducer  from './features/user';
+
 const store = configureStore({
-  reducer: {}
+  reducer: {
+    user: userReducer,
+  }
 })
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider>
+    <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>
